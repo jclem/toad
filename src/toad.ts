8982ab6e02fefe69f97c6273aa07c6a164868902
Jonathan Clem <j@jclem.me>
2023-12-13 22:07:36 -0500

    Revert "Ensure Upgraded is not satisfied by {}"
    This reverts commit a74d19b1b21ac902baf8c329ee4b0716ffdd5c6a.

--- src/toad.ts
+++ src/toad.ts
@@ -7,10 +7,7 @@ export type Middleware<I, O> = (
 
 export type Next<O> = (out: Readonly<O>) => Awaitable<Response | Upgraded>;
 
-class Upgraded {
-  readonly #upgraded = Symbol("upgraded");
-}
-
+class Upgraded {}
 export const upgraded = new Upgraded();
 
 type Awaitable<T> = T | Promise<T>;
